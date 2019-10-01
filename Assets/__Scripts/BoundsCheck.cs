2791c85024bbfd69185e751b30e45e35244b9bac
Maria Deslis <mariadeslis@Marias-MacBook-Pro.local>
2019-10-01 14:05:14 -0600

    improved enemy and boundchecking

--- Assets/__Scripts/BoundsCheck.cs
+++ Assets/__Scripts/BoundsCheck.cs
@@ -6,10 +6,15 @@ public class BoundsCheck : MonoBehaviour
 {
     [Header("Set Inspector")]
     public float radius = 1f;
+    public bool keepOnScreen = true;
 
     [Header("Set Dynamically")]
+    public bool isOnScreen = true;
     public float camWidth;
     public float camHeight;
+
+    [HideInInspector]
+    public bool offRight, offLeft, offUp, offDown;
 
     void Awake()
     {
@@ -21,28 +26,45 @@ public class BoundsCheck : MonoBehaviour
     void LateUpdate()
     {
         Vector3 pos = transform.position;
+        isOnScreen = true;
+        offRight = offLeft = offUp = offDown = false; // this style upsets me
 
         if (pos.x > camWidth - radius)
         {
             pos.x = camWidth - radius;
+            isOnScreen = false;
+            offRight = true;
         }
 
         if (pos.x < -camWidth + radius)
         {
             pos.x = -camWidth + radius;
+            isOnScreen = false;
+            offLeft = true;
         }
 
         if (pos.y > camHeight - radius)
         {
             pos.y = camHeight - radius;
+            isOnScreen = false;
+            offUp = true;
         }
 
         if(pos.y < -camHeight + radius)
         {
             pos.y = -camHeight + radius;
+            isOnScreen = false;
+            offDown = true;
         }
 
-        transform.position = pos;
+        isOnScreen = !(offRight || offLeft || offUp || offDown);
+
+        if (keepOnScreen && !isOnScreen)
+        {
+            transform.position = pos;
+            isOnScreen = true;
+            offRight = offLeft = offDown = offUp = false;
+        }
     }
 
     //Draw the bounds in the Scene pane using onDrawGizmos()
