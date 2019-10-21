03b7950ae44f447f8004d805a6e2a1c619f7f09d
Maria Deslis <mdeslis@gmail.com>
2019-10-21 05:21:14 -0600

    fixed scene and score text again cause it keeps disconnecting @_@, working on powerup still

--- Assets/__Scripts/PowerUp.cs
+++ Assets/__Scripts/PowerUp.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Set in Inspector: Enemy")]
+    public float speed = 10f;   // speed in m/s
+    public float fireRate = .3f;    // Seconds/shots (unused)
+    public float health = 10;
+    public int score = 100; // Points earned for destroying this
+
+    protected BoundsCheck bndCheck;
+
+    private void Awake()
     {
-        
+        bndCheck = GetComponent<BoundsCheck>();
+    }
+
+    // This is a Property : A method that acts like a field
+    public Vector3 pos
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+
+        if (bndCheck != null && !bndCheck.isOnScreen)
+        {
+            // read top to bottom, artemis will rain down
+            if (pos.y < 0 - bndCheck.radius)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
+
+    public virtual void Move()
+    {
+        // top to bottom
+        Vector3 tempPos = pos;
+        tempPos.y -= speed * Time.deltaTime;
+        pos = tempPos;
+    }
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        GameObject otherGO = coll.gameObject;
+        if (otherGO.tag == "Hero")
+        {
+            Destroy(gameObject);    // Destroy the powerup
+            ScoreManager.EVENT(eScoreType.powerUp);
+            //Poor man's debugging
+            //print("Enemy hit by ProjectileHero: " + otherGO.name);
+        }
+        else
+        {
+            print("Enemy hit by non-ProjectileHero: " + otherGO.name);
+        }
+    }
+
+
 }
--- Assets/__Scripts/ScoreManager.cs
+++ Assets/__Scripts/ScoreManager.cs
@@ -7,6 +7,7 @@ public enum eScoreType
 {
     enemy,
     boss,
+    powerUp
 }
 
 public class ScoreManager : MonoBehaviour
